6fab589bb1a210747f160d695ab443a42d8dd604
naa0yama <9667078+naa0yama@users.noreply.github.com>
2026-01-22 20:48:56 +0900

    chore: ass .ssh/rc agent.sock static
    Signed-off-by: naa0yama <9667078+naa0yama@users.noreply.github.com>

--- .bashrc.d/devtool/31-gitconfig-copy.sh
+++ .bashrc.d/devtool/31-gitconfig-copy.sh
@@ -6,23 +6,24 @@ log_warn() { echo -e "\033[0;33m[WARN]\033[0m $*" >&2; }
 log_erro() { echo -e "\033[0;31m[ERRO]\033[0m $*" >&2; }
 
 # Env
-USERPROFILE="$(wslpath -u $(powershell.exe -c '$env:USERPROFILE' | tr -d '\r'))"
+# shellcheck disable=SC2016
+USERPROFILE="$(wslpath -u "$(powershell.exe -c '$env:USERPROFILE' | tr -d '\r')")"
 
 # Copy "~/.gitconfig" from Windows if it doesn't exist
 # File
-if [ ! -f "${HOME}/.gitconfig" -a -f "${USERPROFILE}/.gitconfig" ]; then
+if [ ! -f "${HOME}/.gitconfig" ] && [ -f "${USERPROFILE}/.gitconfig" ]; then
 	log_info "Copy .gitconfig from Windows"
 	cp -v "${USERPROFILE}/.gitconfig" "${HOME}"
 	chmod 0644 "${HOME}/.gitconfig"
 fi
-if [ ! -f "${HOME}/.gitignore_global" -a -f "${USERPROFILE}/.gitignore_global" ]; then
+if [ ! -f "${HOME}/.gitignore_global" ] && [ -f "${USERPROFILE}/.gitignore_global" ]; then
 	log_info "Copy .gitignore_global from Windows"
 	cp -v "${USERPROFILE}/.gitignore_global" "${HOME}"
 	chmod 0644 "${HOME}/.gitignore_global"
 fi
 
 # Directory
-if [ ! -d "${HOME}/.gitconfig.d" -a -d "${USERPROFILE}/.gitconfig.d" ]; then
+if [ ! -d "${HOME}/.gitconfig.d" ] && [ -d "${USERPROFILE}/.gitconfig.d" ]; then
 	log_info "Copy .gitconfig.d from Windows"
 	cp -Rv "${USERPROFILE}/.gitconfig.d" "${HOME}"
 	find "${HOME}/.gitconfig.d" -type d -exec chmod 0755 {} \;
